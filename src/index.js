efd67f99a14de955450815537cf864cb7f247fc0
bogdansolomich <bogdansolomich@gmail.com>
2024-11-18 21:00:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
+import React from 'react';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App />);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
