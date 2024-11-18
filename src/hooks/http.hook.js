6776812a9b8dacd34eded842aca1144f65268b4f
bogdansolomich <bogdansolomich@gmail.com>
2024-11-18 21:08:56 +0200

    Собственный хук на проекте

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react';
-
+// Cобственный хук
 export const useHttp = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
