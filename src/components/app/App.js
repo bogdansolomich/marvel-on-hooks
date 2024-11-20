628f6294b849bef8f81cbcc8298776bf4198e287
bogdansolomich <bogdansolomich@gmail.com>
2024-11-20 15:59:41 +0200

    Маршрутизация

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,37 +1,25 @@
 import AppHeader from '../appHeader/AppHeader';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
-import { useState } from 'react';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+import { MainPage, ComicsPage } from '../pages';
 
-import decoration from '../../resources/img/vision.png';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = id => {
-    setChar(id);
-  };
-
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
-          <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
-          </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <MainPage />
+            </Route>
+            <Route exact path="/comics">
+              <ComicsPage />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 };
 
