06379a603fc94ceb635bd8eb782aa423915f0e50
bogdansolomich <bogdansolomich@gmail.com>
2024-11-20 16:26:53 +0200

    правка

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,9 @@
-import { useState } from 'react';
 import AppHeader from '../appHeader/AppHeader';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
-import ComicsList from '../comicsList/ComicsList';
-import AppBanner from '../appBanner/AppBanner';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+import { MainPage, ComicsPage } from '../pages';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = id => {
-    setChar(id);
-  };
-
   return (
     <Router>
       <div className="app">
@@ -23,22 +11,10 @@ const App = () => {
         <main>
           <Switch>
             <Route exact path="/">
-              <ErrorBoundary>
-                <RandomChar />
-              </ErrorBoundary>
-              <div className="char__content">
-                <ErrorBoundary>
-                  <CharList onCharSelected={onCharSelected} />
-                </ErrorBoundary>
-                <ErrorBoundary>
-                  <CharInfo charId={selectedChar} />
-                </ErrorBoundary>
-              </div>
-              <img className="bg-decoration" src={decoration} alt="vision" />
+              <MainPage />
             </Route>
             <Route exact path="/comics">
-              <AppBanner />
-              <ComicsList />
+              <ComicsPage />
             </Route>
           </Switch>
         </main>
