2e62fc242111a3dce393b02504e195d430e5f2ff
bogdansolomich <bogdansolomich@gmail.com>
2024-12-05 19:45:37 +0200

    Отпр

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,19 @@
-import AppHeader from '../appHeader/AppHeader';
-import { MainPage, ComicsPage, Page404, SingleComicLayout } from '../pages';
-
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import AppHeader from '../appHeader/AppHeader';
+import Spinner from '../spinner/Spinner';
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicLayout = lazy(() =>
+  import('../pages/singleComicLayout/SingleComicLayout')
+);
+const SingleCharacterLayout = lazy(() =>
+  import('../pages/singleCharacterLayout/SingleCharacterLayout')
+);
+const SinglePage = lazy(() => import('../pages/SinglePage'));
 
 const App = () => {
   return (
@@ -10,10 +22,26 @@ const App = () => {
         <AppHeader />
         <main>
           <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/comics" element={<ComicsPage />} />
-            <Route path="/comics/:comicId" element={<SingleComicLayout />} />
-            <Route path="*" element={<Page404 />} />
+            <Suspense fallback={<Spinner />}>
+              <Route path="/">
+                <MainPage />
+              </Route>
+              <Route path="/comics">
+                <ComicsPage />
+              </Route>
+              <Route path="/comics/:id">
+                <SinglePage Component={SingleComicLayout} dataType="comic" />
+              </Route>
+              <Route path="/characters/:id">
+                <SinglePage
+                  Component={SingleCharacterLayout}
+                  dataType="character"
+                />
+              </Route>
+              <Route path="*">
+                <Page404 />
+              </Route>
+            </Suspense>
           </Routes>
         </main>
       </div>
