de8b86417caad51b24f17b2506161d5ab88dbba0
bogdansolomich <bogdansolomich@gmail.com>
2024-12-05 13:11:44 +0200

    Переходы правка

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import AppHeader from '../appHeader/AppHeader';
-import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
+import { MainPage, ComicsPage, Page404, SingleComicLayout } from '../pages';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -12,7 +12,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/comics" element={<ComicsPage />} />
-            <Route path="/comics/:comicId" element={<SingleComicPage />} />
+            <Route path="/comics/:comicId" element={<SingleComicLayout />} />
             <Route path="*" element={<Page404 />} />
           </Routes>
         </main>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
-import React from 'react';
 
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-// root.render(<App />);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
